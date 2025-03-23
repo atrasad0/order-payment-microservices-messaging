e1312d333052ab4483c51a52c8b33940535e5d97
yuri <yuri.jnrgoncalves@gmail.com>
2025-03-22 22:02:58 -0300

    create and configure eureka server

---
+++ op-eureka-server/src/test/java/com/msdemo/opeurekaserver/OpEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msdemo.opeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
