8056f1343f3cae3a779e5c3e00536c9dd72a2638
yuri <yuri.jnrgoncalves@gmail.com>
2025-03-22 23:39:25 -0300

    cofigure eureka client

--- op-order/src/main/java/com/msdemo/oporder/OpOrderApplication.java
+++ op-order/src/main/java/com/msdemo/oporder/OpOrderApplication.java
@@ -3,6 +3,7 @@ package com.msdemo.oporder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OpOrderApplication {
 
