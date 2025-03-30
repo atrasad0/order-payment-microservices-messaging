cc6cfc3477c8712c21282723b0d0b9372ee395f2
yuri <yuri.jnrgoncalves@gmail.com>
2025-03-30 14:58:43 -0300

    api gateway

---
+++ op-api-gateway/src/main/java/com/msdemo/opapigateway/OpApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.msdemo.opapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpApiGatewayApplication.class, args);
+	}
+
+}
---
+++ op-api-gateway/src/test/java/com/msdemo/opapigateway/OpApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msdemo.opapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
